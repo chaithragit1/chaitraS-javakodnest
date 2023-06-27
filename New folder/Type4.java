20158a47d0977a1f4f2b0f0abb0a555e6643dc8d
chaithra <90747040+chaithragit1@users.noreply.github.com>
2023-06-27 16:32:30 +0530

    Type4.java

--- New folder/Type4.java
+++ New folder/Type4.java
@@ -7,4 +7,13 @@ Scanner scan=new Scanner(System.in)
 System.out.println("Enter the value of a:");
 int a=scan.nextInt();
 System.out.println("Enter the value of b:");
-int b=scan.
+int b=scan.nextInt();
+int res=multiply(a,b);
+System.out.println("Multiplication of"+a+"and"+b+"is:"+res);
+}
+  public static int multiply(intx,inty)
+  {
+    int z=x*y;
+    return z;
+  }
+}
